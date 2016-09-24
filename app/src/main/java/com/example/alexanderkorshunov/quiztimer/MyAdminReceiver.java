adca4fb051e8dbeca7b37ff1f84bbc851d5992c6
AKorshunov87 <a.korshunov87@gmail.com>
2016-09-24 20:02:29 +0300

    first commit
    buttons + some logic

---
+++ app/src/main/java/com/example/alexanderkorshunov/quiztimer/MyAdminReceiver.java
@@ -0,0 +1,11 @@
+package com.example.alexanderkorshunov.quiztimer;
+
+/**
+ * Created by Teomir on 24.09.2016.
+ */
+
+import android.app.admin.DeviceAdminReceiver;
+
+public class MyAdminReceiver extends DeviceAdminReceiver{
+
+}

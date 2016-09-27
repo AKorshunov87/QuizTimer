8c529848997f75e6060e9c8e7c7f3712718d9b8d
AKorshunov87 <a.korshunov87@gmail.com>
2016-09-28 00:37:37 +0300

    timer functionality
    timer functionality was implemented. timer tested - not affects settings
    - lock instantly or ater few tries with "handler trick"

--- app/src/main/java/com/example/alexanderkorshunov/quiztimer/MainActivity.java
+++ app/src/main/java/com/example/alexanderkorshunov/quiztimer/MainActivity.java
@@ -1,27 +1,21 @@
 package com.example.alexanderkorshunov.quiztimer;
 
-/*import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-}*/
 import android.app.Activity;
+import android.app.KeyguardManager;
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends Activity implements OnClickListener {
 
@@ -29,6 +23,24 @@ public class MainActivity extends Activity implements OnClickListener {
     private static final String description = "Some Description About Your Admin";
     private DevicePolicyManager mDevicePolicyManager;
     private ComponentName mComponentName;
+    private Handler handler = new Handler();
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+      /* do what you need to do */
+            ButtonEnabled(true);
+            mDevicePolicyManager.lockNow();
+            KeyguardManager keyguardManager = (KeyguardManager)getApplicationContext().getSystemService(Context.KEYGUARD_SERVICE);
+            if( keyguardManager.inKeyguardRestrictedInputMode()) {
+                //it is locked
+            } else {
+                //it is not locked
+                /* and here comes the "trick" */
+                ButtonEnabled(false);
+                handler.postDelayed(this, 100);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,9 +75,19 @@ public class MainActivity extends Activity implements OnClickListener {
             case R.id.btnLock:
                 boolean isAdmin = mDevicePolicyManager.isAdminActive(mComponentName);
                 if (isAdmin) {
-                    //mDevicePolicyManager.lockNow();
-                    mDevicePolicyManager.setMaximumTimeToLock(mComponentName, 2000);
-                }else{
+                    int ms = Integer.parseInt(((EditText)findViewById(R.id.txtBxMs)).getText().toString());
+/*                    Timer timer = new Timer("LockTimer");
+                    timer.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            ButtonEnabled(true);
+                            mDevicePolicyManager.lockNow();
+                        }
+                    }, ms);*/
+
+                    handler.postDelayed(runnable, ms);
+                    ButtonEnabled(false);
+                }else {
                     Toast.makeText(getApplicationContext(), "Not Registered as admin", Toast.LENGTH_SHORT).show();
                 }
                 break;
@@ -77,10 +99,17 @@ public class MainActivity extends Activity implements OnClickListener {
         if (requestCode == ADMIN_INTENT) {
             if (resultCode == RESULT_OK) {
                 Toast.makeText(getApplicationContext(), "Registered As Admin", Toast.LENGTH_SHORT).show();
-            }else{
+            }else {
                 Toast.makeText(getApplicationContext(), "Failed to register as Admin", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    private void ButtonEnabled(boolean isEnabled) {
+        (findViewById(R.id.btnDisableAdmin)).setEnabled(isEnabled);
+        (findViewById(R.id.btnEnableAdmin)).setEnabled(isEnabled);
+        (findViewById(R.id.txtBxMs)).setEnabled(isEnabled);
+        (findViewById(R.id.btnLock)).setEnabled(isEnabled);
     }
 
 }
